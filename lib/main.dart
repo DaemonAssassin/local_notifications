d23c1a2eeeeafef54de39452feb25697b2836757
Mateen <mateendev3@gmail.com>
2022-11-20 13:57:45 +0500

    Learned Basics of Local Notifications in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ import 'package:timezone/timezone.dart' as tz;
 //* 2 => To create notification - create NotificationDetails
 //? - For simple notifications use show method
 //? - For schedule notifications use zoneSchedule method (with Timezone package)
+//* 3 => To handle if app is launched by notification (initial or from bg)
+//? - From Initial -> check checkAppIsLaunchedByNotification() method
+//? - From BG -> check initialize() method of FlutterLocalNotificationsPlugin
+//* 4 => To cancel Notification
+//? - Cancel specific notification using id use -> cancel(id) method
+//? - Cancel all notifications -> cancelAll() method
 
 FlutterLocalNotificationsPlugin notificationsPlugin =
     FlutterLocalNotificationsPlugin();
@@ -33,14 +39,29 @@ void main(List<String> args) async {
     android: androidSettings,
     iOS: iOSSettings,
   );
-  bool? isInitialized =
-      await notificationsPlugin.initialize(initializationSettings);
+  bool? isInitialized = await notificationsPlugin.initialize(
+    initializationSettings,
+    onDidReceiveNotificationResponse: (details) {
+      logger.d(details.payload ?? 'No Payload');
+    },
+  );
   logger.i('Notifications: $isInitialized');
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    checkAppIsLaunchedByNotification();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +70,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(),
         body: Container(),
         floatingActionButton: FloatingActionButton(
-          onPressed: showScheduleNotification,
+          onPressed: showScheduleNotificationWithPayload,
           child: const Icon(Icons.notification_add),
         ),
       ),
@@ -120,5 +141,58 @@ class MyApp extends StatelessWidget {
           UILocalNotificationDateInterpretation.wallClockTime,
       androidAllowWhileIdle: true,
     );
+  }
+
+  void showScheduleNotificationWithPayload() async {
+    AndroidNotificationDetails androidDetails =
+        const AndroidNotificationDetails(
+      'CID-1',
+      'YouTube Notifications',
+      channelDescription: 'This channel is for YouTube Notifications',
+      importance: Importance.max,
+      priority: Priority.max,
+    );
+
+    DarwinNotificationDetails iOSDetails = const DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+      badgeNumber: 0,
+    );
+
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidDetails,
+      iOS: iOSDetails,
+    );
+
+    final DateTime scheduleDate =
+        DateTime.now().add(const Duration(seconds: 5));
+    await notificationsPlugin.zonedSchedule(
+      0,
+      'Zoned Schedule Notification',
+      'Data is here.',
+      tz.TZDateTime.from(scheduleDate, tz.local),
+      notificationDetails,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.wallClockTime,
+      androidAllowWhileIdle: true,
+      payload: 'IAmPayload',
+    );
+  }
+
+  /// To handle app - if the app is launched by notification (initially)
+  void checkAppIsLaunchedByNotification() async {
+    NotificationAppLaunchDetails? details =
+        await notificationsPlugin.getNotificationAppLaunchDetails();
+
+    if (details != null) {
+      if (details.didNotificationLaunchApp) {
+        NotificationResponse? notificationResponse =
+            details.notificationResponse;
+        if (notificationResponse != null) {
+          logger.d(notificationResponse.payload ?? 'No Data');
+        }
+      }
+    }
   }
 }
