96fce152bb253dce6cd9355edf76282553b2703e
Mateen <mateendev3@gmail.com>
2022-11-20 13:28:32 +0500

    Learned ZonedSchedule Notifications - with TimeZone package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:logger/logger.dart';
+import 'package:timezone/data/latest_10y.dart';
+import 'package:timezone/timezone.dart' as tz;
+
+//! Create Notifications
+//* 1 => To initialize local notifications - create InitializationSettings
+//* 2 => To create notification - create NotificationDetails
+//? - For simple notifications use show method
+//? - For schedule notifications use zoneSchedule method (with Timezone package)
 
 FlutterLocalNotificationsPlugin notificationsPlugin =
     FlutterLocalNotificationsPlugin();
@@ -8,6 +16,8 @@ Logger logger = Logger();
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  initializeTimeZones(); // package -> timezone/data/latest_10y.dart
   AndroidInitializationSettings androidSettings =
       const AndroidInitializationSettings('@mipmap/ic_launcher');
   DarwinInitializationSettings iOSSettings = const DarwinInitializationSettings(
@@ -38,7 +48,77 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(),
         body: Container(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: showScheduleNotification,
+          child: const Icon(Icons.notification_add),
+        ),
       ),
+    );
+  }
+
+  void showNotification() async {
+    AndroidNotificationDetails androidDetails =
+        const AndroidNotificationDetails(
+      'CID-1',
+      'YouTube Notifications',
+      channelDescription: 'This channel is for YouTube Notifications',
+      importance: Importance.max,
+      priority: Priority.max,
+    );
+
+    DarwinNotificationDetails iOSDetails = const DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+      badgeNumber: 0,
+    );
+
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidDetails,
+      iOS: iOSDetails,
+    );
+
+    await notificationsPlugin.show(
+      0,
+      'First Notification',
+      'Hello To the Future',
+      notificationDetails,
+    );
+  }
+
+  void showScheduleNotification() async {
+    AndroidNotificationDetails androidDetails =
+        const AndroidNotificationDetails(
+      'CID-1',
+      'YouTube Notifications',
+      channelDescription: 'This channel is for YouTube Notifications',
+      importance: Importance.max,
+      priority: Priority.max,
+    );
+
+    DarwinNotificationDetails iOSDetails = const DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+      badgeNumber: 0,
+    );
+
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidDetails,
+      iOS: iOSDetails,
+    );
+
+    final DateTime scheduleDate =
+        DateTime.now().add(const Duration(seconds: 5));
+    await notificationsPlugin.zonedSchedule(
+      0,
+      'Zoned Schedule Notification',
+      'Data is here.',
+      tz.TZDateTime.from(scheduleDate, tz.local),
+      notificationDetails,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.wallClockTime,
+      androidAllowWhileIdle: true,
     );
   }
 }
